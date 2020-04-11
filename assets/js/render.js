3272aa838ff129997aeaed2ba25c30b13709629e
MitaMasaki <mimimita3131@gmail.com>
2020-04-11 15:03:54 +0900

    If the device access by smartphones, hide drawing the background

--- assets/js/render.js
+++ assets/js/render.js
@@ -1,11 +1,26 @@
 $(function () {
     resize();
-    draw();
+    if(getDevice() !== 'sp') {
+        draw();
+    }
     $(window).resize(function () {
         resize();
-        draw();
+        if(getDevice() !== 'sp') {
+            draw();
+        }
     });
 });
+
+function getDevice(){
+    const ua = navigator.userAgent;
+    if(ua.indexOf('iPhone') > 0 || ua.indexOf('iPod') > 0 || ua.indexOf('Android') > 0 && ua.indexOf('Mobile') > 0){
+        return 'sp';
+    }else if(ua.indexOf('iPad') > 0 || ua.indexOf('Android') > 0){
+        return 'tab';
+    }else{
+        return 'other';
+    }
+};
 
 function resize() {
     let background = $('#background');
